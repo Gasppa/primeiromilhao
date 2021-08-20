9b8033dd3f24a8e871c065042084e465aeb0155d
Victor <victor.gasparoni@sysmap.com.br>
2021-08-20 05:23:39 -0300

    Connected frontend with backend and fixed the form validation

--- frontend/src/containers/components/Form/index.js
+++ frontend/src/containers/components/Form/index.js
@@ -4,6 +4,7 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import { withStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
+import { getFirstMillionProjectionUsingAxios } from '../../../core/api'
 
 import './index.css'
 
@@ -38,9 +39,12 @@ const initialState = {
   email: '',
   gender: 'feminino',
   dateOfBirth: new Date().toISOString().split('T')[0],
-  initialValue: 1,
-  yearsToAccomplish: 1,
-  IsFormValid: true
+  initialValue: '',
+  yearsToAccomplish: '',
+  IsFormValid: true,
+  IsModalOpen: false,
+  pmtComParMais: 0,
+  pmtSemParMais: 0
 }
 
 export default class Form extends Component {
@@ -49,6 +53,37 @@ export default class Form extends Component {
     this.state = initialState
     this.handleChange = this.handleChange.bind(this)
     this.handleDateChange = this.handleDateChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  async handleSubmit (e) {
+    e.preventDefault()
+    const IsFormValid =
+      this.state.name !== '' &&
+      this.state.email !== '' &&
+      this.state.dateOfBirth !== '' &&
+      this.state.initialValue >= 0 &&
+      this.state.yearsToAccomplish > 0
+
+    console.log(IsFormValid)
+    this.setState((state) => {
+      return { ...state, IsFormValid }
+    })
+    console.log(this.state)
+
+    if (IsFormValid) {
+      const { initialValue, yearsToAccomplish } = this.state
+      const body = { initialValue, yearsToAccomplish }
+      const result = await getFirstMillionProjectionUsingAxios(body)
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          pmtComParMais: result.data.pmtComParMais,
+          pmtSemParMais: result.data.pmtSemParMais,
+          IsModalOpen: !IsFormValid
+        }
+      })
+    }
   }
 
   handleChange (e) {
@@ -83,7 +118,7 @@ export default class Form extends Component {
     return (
       <>
         <div className="form_container">
-          <form className="form-parmais">
+          <form className="form-parmais" onSubmit={this.handleSubmit}>
             <div className="form_card">
               <div className="form_header">
                 <h1 className="ff-mulish_black">Como atingir R$ 1 milhão</h1>
@@ -136,27 +171,29 @@ export default class Form extends Component {
               <CustomTextField
                 className="input-parmais" type="number" name="initialValue" label="Quanto você tem para investir hoje?"
                 onChange={this.handleChange}
-                error={this.state.initialValue < 0 && !this.state.IsFormValid}
+                value={this.state.initialValue}
+                error={(this.state.initialValue < 0 || this.state.initialValue === '') && !this.state.IsFormValid}
               />
               <div className="mandatory-field_container">
                 <span
                   className='ff-mulish_regular mandatory-field_text'
-                  hidden={this.state.initialValue > 0 || this.state.IsFormValid}>* O valor deve ser maior ou igual a 0</span>
+                  hidden={(this.state.initialValue >= 0 && this.state.initialValue !== '') || this.state.IsFormValid}>* O valor não deve ser negativo</span>
               </div>
               <CustomTextField
                 className="input-parmais" type="number" name="yearsToAccomplish" label="Daqui quantos anos você quer atingir?"
                 onChange={this.handleChange}
+                value={this.state.yearsToAccomplish}
                 error={this.state.yearsToAccomplish <= 0 && !this.state.IsFormValid}
               />
               <div className="mandatory-field_container">
                 <span
                   className='ff-mulish_regular mandatory-field_text'
-                  hidden={this.state.yearsToAccomplish >= 0 || this.state.IsFormValid}>* O valor deve ser maior que 0</span>
+                  hidden={(this.state.yearsToAccomplish > 0 && this.state.yearsToAccomplish !== '') || this.state.IsFormValid}>* O valor deve ser maior que 0</span>
               </div>
               <span className="ff-mulish_regular medium-gray form_disclaimer">Nesta simulação aplicamos uma taxa de juros real de 4,0% ao ano e uma inflação de 4,8% ao ano.</span>
 
             </div>
-            <button className="button-parmais button-parmais_md ff-mulish_bold" type="submit">Cadastrar</button>
+            <button className="button-parmais button-parmais_md ff-mulish_bold" type="submit">Calcular</button>
           </form>
         </div>
       </>
--- frontend/src/core/api/index.js
+++ frontend/src/core/api/index.js
@@ -1,4 +1,5 @@
 import Swagger from 'swagger-client'
+import axios from 'axios'
 
 const BACKEND_PORT = 8080
 
@@ -9,4 +10,16 @@ export const getProjections = () => {
     return client.apis.data.getProjections()
       .then(({ body }) => body)
   })
+}
+
+export const getFirstMillionProjection = () => {
+  return swagger.then(client => {
+    return client.apis.myfirstmillion.getFirstMillionProjection()
+      .then(({ body }) => body)
+  })
+}
+
+export const getFirstMillionProjectionUsingAxios = async (body) => {
+  const result = await axios.post('http://localhost:8080/api/myfirstmillion/pmt', { ...body })
+  return result
 }
