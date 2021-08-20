799510a68575e7da821240688ec0e0dce39cae7d
Victor <victor.gasparoni@sysmap.com.br>
2021-08-20 02:44:29 -0300

    Added Footer component

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Form from '../components/Form'
+import Footer from '../components/Footer'
 
 import './index.css'
 
@@ -9,6 +10,7 @@ function App () {
     <>
       <Header/>
       <Form/>
+      <Footer/>
     </>
   )
 }
---
+++ frontend/src/containers/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../../../assets/images/parmais-logo.png'
+import './index.css'
+
+function Footer () {
+  return (
+    <div className="footer_parmais">
+      <div className="footer-div logo">
+        <img className="footer_parmais_logo" src={logo}/>
+      </div>
+      <div className="footer-div name">
+          <span className="ff-mulish_black footer_parmais_name">ParMais Investimentos Financeiros</span>
+      </div>
+      <div className="footer-div cnpj">
+        <span className="ff-mulish_regular footer_parmais_cnpj">PAR MAIS GESTÃO ADMINISTRAÇÃO DE VALORES MOBILIÁRIOS
+  CNPJ: 21.719.643/0001-60</span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
